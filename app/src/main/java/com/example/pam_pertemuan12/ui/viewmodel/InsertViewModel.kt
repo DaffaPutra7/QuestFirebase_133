67b6fe5681656f96933b7d179d5ebf513de495ff
DaffaPutra7 <putradika3604@gmail.com>
2025-01-11 21:59:25 +0700

    Menambah code pada file InsertViewModel.kt berupa data class InsertViewModel untuk mengelola data input, validasi, dan menyimpan data mahasiswa serta melakukan pengubahan code

--- app/src/main/java/com/example/pam_pertemuan12/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pam_pertemuan12/ui/viewmodel/InsertViewModel.kt
@@ -1,6 +1,71 @@
 package com.example.pam_pertemuan12.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.pam_pertemuan12.model.Mahasiswa
+import com.example.pam_pertemuan12.repository.RepositoryMhs
+import kotlinx.coroutines.launch
+
+class InsertViewModel (
+    private val mhs: RepositoryMhs
+) : ViewModel() {
+    var uiEvent: InsertUiState by mutableStateOf(InsertUiState())
+        private set
+
+    var uiState: FormState by mutableStateOf(FormState.Idle)
+        private set
+
+    // Memperbarui state berdasarkan input pengguna
+    fun updateState(mahasiswaEvent: MahasiswaEvent) {
+        uiEvent = uiEvent.copy(
+            insertUiEvent = mahasiswaEvent
+        )
+    }
+
+    // Validasi data input pengguna
+    fun validateFields() : Boolean {
+        val event = uiEvent.insertUiEvent
+        val errorState = FormErrorState(
+            nim = if (event.nim.isNotEmpty()) null else "NIM tidak boleh kosong",
+            nama = if (event.nama.isNotEmpty()) null else "Nama tidak boleh kosong",
+            alamat = if (event.alamat.isNotEmpty()) null else "Alamat tidak boleh kosong",
+            kelas = if (event.kelas.isNotEmpty()) null else "Kelas tidak boleh kosong",
+            gender = if (event.gender.isNotEmpty()) null else "Jenis Kelamin tidak boleh kosong",
+            angkatan = if (event.angkatan.isNotEmpty()) null else "Angkatan tidak boleh kosong"
+        )
+
+        uiEvent = uiEvent.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+
+    fun insertMhs() {
+        if (validateFields()) {
+            viewModelScope.launch {
+                uiState = FormState.Loading
+                try {
+                    mhs.insertMhs(uiEvent.insertUiEvent.toMhsModel())
+                    uiState = FormState.Success("Data berhasil disimpan")
+                } catch (e: Exception) {
+                    uiState = FormState.Error("Data gagal disimpan")
+                }
+            }
+        } else {
+            uiState = FormState.Error("Data tidak valid")
+        }
+    }
+
+    fun resetForm() {
+        uiEvent = InsertUiState()
+        uiState = FormState.Idle
+    }
+
+    fun resetSnackBarMessage() {
+        uiState = FormState.Idle
+    }
+}
 
 sealed class FormState {
     object Idle : FormState()
@@ -17,14 +82,14 @@ data class InsertUiState(
 data class FormErrorState (
     val nim: String? = null,
     val nama: String? = null,
-    val jenisKelamin: String? = null,
     val alamat: String? = null,
     val kelas: String? = null,
+    val gender: String? = null,
     val angkatan: String? = null
 ) {
     fun isValid() : Boolean {
-        return nim == null && nama == null && jenisKelamin == null &&
-                alamat == null && kelas == null && angkatan == null
+        return nim == null && nama == null && alamat == null &&
+                kelas == null && gender == null && angkatan == null
     }
 }
 
@@ -32,9 +97,9 @@ data class FormErrorState (
 data class MahasiswaEvent (
     val nim: String = "",
     val nama: String = "",
-    val jenisKelamin: String = "",
     val alamat: String = "",
     val kelas: String = "",
+    val gender: String = "",
     val angkatan: String = ""
 )
 
@@ -42,8 +107,8 @@ data class MahasiswaEvent (
 fun MahasiswaEvent.toMhsModel(): Mahasiswa = Mahasiswa (
     nim = nim,
     nama = nama,
-    jenisKelamin = jenisKelamin,
     alamat = alamat,
     kelas = kelas,
+    gender = gender,
     angkatan = angkatan
 )
