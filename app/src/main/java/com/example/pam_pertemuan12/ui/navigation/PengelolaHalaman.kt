23f5ce4be077af0c8c4f6cb5a84946a7bd971ea5
DaffaPutra7 <putradika3604@gmail.com>
2025-01-12 20:03:25 +0700

    Membuat file PengelolaHalaman.kt didalam package navigation yang digunakan untuk menghubungkan navigasi antar layar Home dan Insert

---
+++ app/src/main/java/com/example/pam_pertemuan12/ui/navigation/PengelolaHalaman.kt
@@ -0,0 +1,39 @@
+package com.example.pam_pertemuan12.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.pam_pertemuan12.ui.view.HomeScreen
+import com.example.pam_pertemuan12.ui.view.InsertView
+
+@Composable
+fun PengelolaHalaman(
+    modifier: Modifier,
+    navController: NavHostController = rememberNavController()
+){
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToltemEntry = {
+                    navController.navigate(DestinasiInsert.route)
+                },
+            )
+        }
+
+        composable(DestinasiInsert.route) {
+            InsertView(
+                onBack = { navController.popBackStack() },
+                onNavigate = {
+                    navController.navigate(DestinasiHome.route)
+                }
+            )
+        }
+    }
+}
